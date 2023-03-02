e15e80e4fde0deb9d89abf65cd2097cbf89d9d99
kilroy80 <kilroy80@gmail.com>
2023-03-02 15:57:20 +0900

    feat: Add check func

--- lib/flutter_app_signature.dart
+++ lib/flutter_app_signature.dart
@@ -1,8 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 
 import 'flutter_app_signature_platform_interface.dart';
 
 class FlutterAppSignature {
   Future<List<String>> getAppSignature() {
     return FlutterAppSignaturePlatform.instance.getAppSignature();
+  }
+
+  Future<bool> checkAppSignature(List<String> allowList) async {
+    if (!kIsWeb && Platform.isAndroid) {
+      var appSignatureList = await FlutterAppSignature().getAppSignature();
+
+      var result = allowList.where((element) => appSignatureList.contains(element));
+      if (result.isEmpty) {
+        // disallow
+        return false;
+      } else {
+        // allow
+        return true;
+      }
+    } else {
+      // other platform always true
+      return true;
+    }
   }
 }
